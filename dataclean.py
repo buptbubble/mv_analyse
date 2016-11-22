eee3cc483794e2732d91ad248744513809c9f248
buptbubble <buptliyuchen@163.com>
2016-11-22 23:21:55 +0800

    213

--- dataclean.py
+++ dataclean.py
@@ -4,61 +4,155 @@ import pandas as pd
 datapath = './data'
 dataname = '32_BasketballPass_416x240_50.txt'
 
-def file_analyser(filepath):
-    datadf = pd.DataFrame
+def loadToDf(filepath,sample=-1):
+
     infoSet = {}
     datacount = 0
+    mergeInfoFlag = 0
     for i,line in enumerate(open(filepath,'r')):
         line = line.strip('\n')
         lineList = line.split(',')
         flag = lineList[0]
+
         if flag == '$':
             if i != 0:
                 if 'merFlag' not in infoSet.keys():
-                    infoSet['merFlag'] = -1
-
-                pdrecord = pd.DataFrame(infoSet,index=[0])
-                if datadf.empty:
-                    datadf = pdrecord.copy()
-                else:
-                    pd.concat(datadf,pdrecord)
-
-                print datadf
+                    infoSet['merFlag'] = []
+                if mergeInfoFlag == 0:
+                    infoSet['merFlag'].append(-1)
+                mergeInfoFlag = 0
                 datacount+=1
-                if datacount==5:
+                if sample != -1:
+                    if datacount==sample:
+                        break
 
-                    break
-                infoSet.clear()
-            infoSet['poc'] =lineList[1]
-            infoSet['cusize'] = lineList[2]
-            infoSet['ps'] = lineList[3]
-            infoSet['puIdx'] = lineList[4]
+            if 'poc' not in infoSet.keys():
+                infoSet['poc'] = []
+                infoSet['poc'].append(int(lineList[1]))
+            else:
+                infoSet['poc'].append(int(lineList[1]))
+            if 'cusize' not in infoSet.keys():
+                infoSet['cusize'] = []
+                infoSet['cusize'].append(int(lineList[2]))
+            else:
+                infoSet['cusize'].append(int(lineList[2]))
+
+            if 'ps' not in infoSet.keys():
+                infoSet['ps'] = []
+                infoSet['ps'].append(int(lineList[3]))
+            else:
+                infoSet['ps'].append(int(lineList[3]))
+
+            if 'puIdx' not in infoSet.keys():
+                infoSet['puIdx'] = []
+                infoSet['puIdx'].append(int(lineList[4]))
+            else:
+                infoSet['puIdx'].append(int(lineList[4]))
+
+
         if flag == '#':
+            if 'biType' not in infoSet.keys():
+                infoSet['biType'] = []
             if lineList[1] == 'uni':
-                infoSet['biType'] = 'u'
+                infoSet['biType'].append('u')
 
             elif lineList[1] == 'non-Bi':
-                infoSet['biType'] = 'm'
+                infoSet['biType'].append('m')
 
             elif lineList[1] == 'Bi':
-                infoSet['biType'] = 'b'
+                infoSet['biType'].append('b')
+
         if flag == '0':
             refNum = int(lineList[1])
-            infoSet['l0_mvx'] = int(lineList[2+refNum])
-            infoSet['l0_mvy'] = int(lineList[3+refNum])
-            infoSet['l0_refBestPoc'] = int(lineList[4+refNum])
-            infoSet['l0_cost'] = int(lineList[5+refNum])
+            if 'l0_mvx' not in infoSet.keys():
+                infoSet['l0_mvx'] = []
+                infoSet['l0_mvx'].append(int(lineList[2+refNum]))
+            else:
+                infoSet['l0_mvx'].append(int(lineList[2+refNum]))
+
+            if 'l0_mvy' not in infoSet.keys():
+                infoSet['l0_mvy'] = []
+                infoSet['l0_mvy'].append(int(lineList[3+refNum]))
+            else:
+                infoSet['l0_mvy'].append(int(lineList[3+refNum]))
+
+            if 'l0_refBestPoc' not in infoSet.keys():
+                infoSet['l0_refBestPoc'] = []
+                infoSet['l0_refBestPoc'].append(int(lineList[4+refNum]))
+            else:
+                infoSet['l0_refBestPoc'].append(int(lineList[4+refNum]))
+
+            if 'l0_cost' not in infoSet.keys():
+                infoSet['l0_cost'] = []
+                infoSet['l0_cost'].append(int(lineList[5+refNum]))
+            else:
+                infoSet['l0_cost'].append(int(lineList[5+refNum]))
+
+
         if flag == '1':
             refNum = int(lineList[1])
-            infoSet['l1_mvx'] = int(lineList[2 + refNum])
-            infoSet['l1_mvy'] = int(lineList[3 + refNum])
-            infoSet['l1_refBestPoc'] = int(lineList[4 + refNum])
-            infoSet['l1_cost'] = int(lineList[5 + refNum])
+
+            if 'l1_mvx' not in infoSet.keys():
+                infoSet['l1_mvx'] = []
+                infoSet['l1_mvx'].append(int(lineList[2 + refNum]))
+            else:
+                infoSet['l1_mvx'].append(int(lineList[2 + refNum]))
+
+            if 'l1_mvy' not in infoSet.keys():
+                infoSet['l1_mvy'] = []
+                infoSet['l1_mvy'].append(int(lineList[3 + refNum]))
+            else:
+                infoSet['l1_mvy'].append(int(lineList[3 + refNum]))
+
+            if 'l1_refBestPoc' not in infoSet.keys():
+                infoSet['l1_refBestPoc'] = []
+                infoSet['l1_refBestPoc'].append(int(lineList[4 + refNum]))
+            else:
+                infoSet['l1_refBestPoc'].append(int(lineList[4 + refNum]))
+
+            if 'l1_cost' not in infoSet.keys():
+                infoSet['l1_cost'] = []
+                infoSet['l1_cost'].append(int(lineList[5 + refNum]))
+            else:
+                infoSet['l1_cost'].append(int(lineList[5 + refNum]))
+
         if flag == '&':
+            mergeInfoFlag = 1
+            if 'merFlag' not in infoSet.keys():
+                infoSet['merFlag']=[]
             if lineList[1] == 'Merge':
-                infoSet['merFlag'] = 1
-            if lineList[1] == 'NotMerge':
-                infoSet['merFlag'] = 0
+                infoSet['merFlag'].append(1)
+            else:
+                infoSet['merFlag'].append(0)
+    #print infoSet['merFlag']
+    #print infoSet['biType']
+    feaLen = []
+    for key in infoSet.keys():
+        feaLen.append(len(infoSet[key]))
+    for x in feaLen:
+        if feaLen[0] != x:
+            print "Feather amount error!"
+            exit(1)
+    datadf = pd.DataFrame.from_dict(infoSet)
+    return datadf
+
+
+
+def saveDataToFiles(filepath):
+    filename = filepath.split('_')[1]
+    savepath = os.path.join(datapath,filename)
+    if not os.path.exists(savepath):
+        os.mkdir(savepath)
+    df = loadToDf(filepath)
+
+    paraset = [64,32,16,8]
+    for item in paraset:
+        df_sel = df[df.cusize == item]
+        df_sel.index = range(df_sel.shape[0])
+        w_name = 'cu'+str(item)+'.txt'
+        df_sel.to_csv(os.path.join(savepath,w_name))
+
+
 
 
 
@@ -70,5 +164,19 @@ def file_analyser(filepath):
 
 
 if __name__ == '__main__':
-    filepath = os.path.join(datapath,dataname)
-    file_analyser(filepath)
+
+
+
+    files = []
+    for name in os.listdir(datapath):
+        ext = os.path.splitext(name)[1]
+        if ext != '':
+            files.append(name)
+
+    for i,filename in enumerate(files):
+        print str(i+1)+'.'+filename
+        filepath = os.path.join(datapath,filename)
+        saveDataToFiles(filepath)
+
+    exit(0)
+
